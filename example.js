b4f56c066949e370a5bca4323308fe23640c8c22
Jacob Jordan <JORDAN_J@afi.afiama.com>
2016-08-17 10:08:18 -0500

    rewritten to work with update

--- example.js
+++ example.js
@@ -1,8 +1,14 @@
-var GiantBomb = require("giant-bomb");
+var GiantBomb = require('giant-bomb');
 
 //Get API key at http://giantbomb.com/api
-var gb = new GiantBomb('your api key goes here', 'agent');
+var gb = new GiantBomb('your api key', 'your unique user agent');
 
-gb.search('Metal Gear', 0, function(err, response, items){
-    items.results.forEach(game => {console.log(game.name)});
-});
+gb.search({query: 'Persona', fields: ['name'], limit: 10, resources: ['game']}, 
+	function(error, response, body){
+		if(!error && response.statusCode == 200){
+			body.results.forEach(game => {
+				console.log(game.name);
+			});
+		}
+	}
+);
